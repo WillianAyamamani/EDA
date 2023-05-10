f6933981861be26ca7ba2bfc3a80e0557f53fde1
Francis Ayamamani <cpumal@unsa.edu.pe>
2023-05-10 13:25:53 -0500

    tipos de variables

--- variable.java
+++ variable.java
@@ -3,7 +3,14 @@ public static void main (String[]args){
 //imprime variables
 String nombre="PEPE";
 System.out.println(nombre);
-//Operadores aritmeticos
+//Operadores aritmetics
+
 System.out.println(12+12);
+//Tipos de variables;
+int num=5;
+float floa=5.99f;
+char c= 'D';
+boolean valor=true;
+String dato="Cierto"
 }
 }
