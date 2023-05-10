404bd28b88d25c0056ef493efa751e49450bd8d8
Francis Ayamamani <cpumal@unsa.edu.pe>
2023-05-10 13:29:33 -0500

    tipos de operadores en int

---
+++ operadores.java
@@ -0,0 +1,12 @@
+public class operadores{
+public static void main (String[]args){
+int num=5+2;
+System.out.println(num);
+int num=5-2;
+System.out.println(num);
+int num=5*2;
+System.out.println(num);
+int num=5/2;
+System.out.println(num);
+}
+}
